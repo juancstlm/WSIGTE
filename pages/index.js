3952db4486359bd7c534896aa1517804392557fb
Juan Castillo <juancstl@protonmail.com>
2021-02-02 18:42:01 -0800

    Add refresh feature

--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,22 @@ import { Map, MapkitProvider, Marker, useMap } from 'react-mapkit';
 
 export default function Home() {
 
-  const renderLoadingScreen = () => {
-    return (<div style={{height: '100vh', width: '100vh'}}><h1>Loading</h1></div>)
-  }
-
   const UseMapExample = () => {
     const { map, mapProps, setCenter, mapkit, setRegion, setVisibleMapRect } = useMap({showsUserLocation: true})
-    const [position, setPosition] = useState();
+    const [position, setPosition] = useState()
     const [userCoordinates, setUserCoordinates] = useState()
+    const [results, setResults] = useState()
+    const [status, setStatus] =  useState('Getting your location')
+    const [placeAnnotation, setPlaceAnnotation] = useState()
+    const [path, setPath] = useState()
 
     useEffect(()=>{
       if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(getPosition);
-          }
+      }
           function getPosition(pos) {
-            console.log(pos.coords.latitude, pos.coords.longitude);
             setPosition(pos)
-          }
+      }
     }, [])
 
     useEffect(()=>{
@@ -32,33 +31,52 @@ export default function Home() {
         )
         setUserCoordinates(userCoordinate)
         let region = new mapkit.CoordinateRegion(userCoordinate);
-        console.log(region)
         setRegion(region)
         setCenter([position.coords.latitude,
           position.coords.longitude])
+        searchForPlacesToEat()
       }
 
     }, [mapkit, position])
 
+    const renderLoadingScreen = () => {
+      if (!results) {
+        return (<div style={{height: '100%', width: '100%', position: 'absolute', zIndex: 100, backgroundColor: 'white'}}>
+          <h1><a href="https://github.com/juancstlm/wthsige" >Where Should I Go To Eat</a></h1>
+          <h2>Loading</h2>
+          <h3>{status}</h3>
+        </div>)
+      } return null;
+    }
+
     const searchForPlacesToEat = () => {
+      setStatus('Looking for Places')
       const searchParams = '93905';
+
+      if(placeAnnotation){
+        map.removeAnnotation(placeAnnotation)
+      }
+
+      if(path){
+        map.removeItems(path)
+      }
 
       // let span = new mapkit.CoordinateSpan(0.016, 0.016);
       let filters = new mapkit.PointOfInterestFilter
         .including([mapkit.PointOfInterestCategory.Bakery, mapkit.PointOfInterestCategory.Cafe ,mapkit.PointOfInterestCategory.Restaurant])
 
+      console.log(userCoordinates)
+
+      let userCoordinate = new mapkit.Coordinate(
+        position.coords.latitude,
+        position.coords.longitude
+      )
+
       let pointOfInterestSearch = new mapkit.PointsOfInterestSearch({
-        center: userCoordinates,
+        center: userCoordinate,
         pointOfInterestFilter: filters,
         radius: 3000,
       })
-
-      // let search = new mapkit.Search({
-      //   getsUserLocation: true,
-      //   region: mapProps.region,
-      //   includePointsOfInterest: true,
-      //   pointOfInterestFilter:
-      // })
 
       pointOfInterestSearch.search((error, data) => {
         if (error) {
@@ -67,13 +85,15 @@ export default function Home() {
         }
 
         // Place the users location on the map via a MarkerAnnotation
-        // let userAnnotation = new mapkit.MarkerAnnotation(userCoordinate);
-        // userAnnotation.color = "#f96345";
-        // userAnnotation.glyphText = "🏠";
-        // map.addAnnotation(userAnnotation);
+        let userAnnotation = new mapkit.MarkerAnnotation(userCoordinate);
+        userAnnotation.color = "#f96345";
+        userAnnotation.glyphText = "🏠";
+        map.addAnnotation(userAnnotation);
 
         // Pick a random place from the list of search results
         let randomIndex = Math.floor(Math.random() * data.places.length);
+        setResults(data.places);
+        setStatus('Results found');
         let randomPlace = data.places[randomIndex];
         let randomPlaceAnnotation = new mapkit.MarkerAnnotation(
           randomPlace.coordinate
@@ -83,11 +103,12 @@ export default function Home() {
         randomPlaceAnnotation.subtitle = randomPlace.formattedAddress;
 
         // add the annotation to the map
+        setPlaceAnnotation(randomPlaceAnnotation);
         map.addAnnotation(randomPlaceAnnotation);
 
         let route = new mapkit.Directions().route(
           {
-            origin: userCoordinates,
+            origin: userCoordinate,
             destination: randomPlace
           },
           (error, data) => {
@@ -100,13 +121,15 @@ export default function Home() {
               });
             });
 
+            setPath(polylines)
+
             map.showItems(polylines, {
               animate: true,
               padding: new mapkit.Padding({
                 top: 200,
-                right: 50,
+                right: 56,
                 bottom: 100,
-                left: 50
+                left: 56
               })
             });
           }
@@ -116,14 +139,17 @@ export default function Home() {
 
     return (
       <>
-        <h1><a href="https://github.com/juancstlm/wthsige" >Where should I go to eat?</a></h1>
-        {/*<button onClick={searchForPlacesToEat}>Search</button>*/}
-        <div id={'test'} style={{width: '100%', margin: '0 auto', height: '100vh' }}>
-          <Map {...mapProps}  />
+        <h1>Why dont you eat Here?</h1>
+        {renderLoadingScreen()}
+        <div id={'test'} style={{ width: '100%', margin: '0 auto', height: '100vh' }}>
+          <Map {...mapProps} />
         </div>
+        {results ? <div style={{position: 'absolute', bottom: 100, zIndex: 10, left: 200}}>
+          <button onClick={searchForPlacesToEat}>No! That Place Looks Awful</button>
+        </div> : null}
       </>
     )
-  }
+  };
 
 
   return (
