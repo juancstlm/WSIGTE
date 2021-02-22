08f0c5c2b0d570bba01fb2adf9fe0f64326c0962
Juan Castillo <juancstl@protonmail.com>
2021-02-21 23:50:47 -0800

    Fix apple maps and geolocation

--- pages/index.js
+++ pages/index.js
@@ -6,38 +6,44 @@ import { Map, MapkitProvider, Marker, useMap } from 'react-mapkit';
 export default function Home() {
 
   const UseMapExample = () => {
-    const { map, mapProps, setCenter, mapkit, setRegion, setVisibleMapRect } = useMap({showsUserLocation: true})
-    const [position, setPosition] = useState()
+    const { map, mapProps, setCenter, mapkit, setRegion, setVisibleMapRect } = useMap({showsUserLocation: true, tracksUserLocation: true})
     const [userCoordinates, setUserCoordinates] = useState()
     const [results, setResults] = useState()
     const [status, setStatus] =  useState('Getting your location')
     const [placeAnnotation, setPlaceAnnotation] = useState()
     const [path, setPath] = useState()
+    const [eventListeners, setEventListeners] = useState([])
+    const [radius, setRadius] = useState(3000);
 
     useEffect(()=>{
-      if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(getPosition);
+      if (map && eventListeners.length < 3) {
+        map.addEventListener('user-location-change', handleUserLocationChange)
+        setEventListeners([...eventListeners, 'user-location-change'])
+        map.addEventListener('user-location-error',handleUserLocationError)
+        setEventListeners([...eventListeners, 'user-location-error'])
       }
-          function getPosition(pos) {
-            setPosition(pos)
-      }
-    }, [])
+    }, [map])
+
+    const handleUserLocationChange = (event) => {
+      const {coordinate, timestamp} = event
+      setUserCoordinates(coordinate)
+      console.log('User Location Changed');
+    }
+
+    const handleUserLocationError = (error) => {
+      console.warn(`Error ${error.code}, ${error.message}`)
+    }
 
     useEffect(()=>{
-      if(mapkit && position){
-        let userCoordinate = new mapkit.Coordinate(
-          position.coords.latitude,
-          position.coords.longitude
-        )
-        setUserCoordinates(userCoordinate)
-        let region = new mapkit.CoordinateRegion(userCoordinate);
+      if(mapkit && userCoordinates){
+        let region = new mapkit.CoordinateRegion(userCoordinates);
         setRegion(region)
-        setCenter([position.coords.latitude,
-          position.coords.longitude])
+        setCenter([userCoordinates.latitude,
+          userCoordinates.longitude])
         searchForPlacesToEat()
       }
 
-    }, [mapkit, position])
+    }, [mapkit, userCoordinates, radius])
 
     const renderLoadingScreen = () => {
       if (!results) {
@@ -65,75 +71,75 @@ export default function Home() {
       let filters = new mapkit.PointOfInterestFilter
         .including([mapkit.PointOfInterestCategory.Bakery, mapkit.PointOfInterestCategory.Cafe ,mapkit.PointOfInterestCategory.Restaurant])
 
-      console.log(userCoordinates)
-
-      let userCoordinate = new mapkit.Coordinate(
-        position.coords.latitude,
-        position.coords.longitude
-      )
-
       let pointOfInterestSearch = new mapkit.PointsOfInterestSearch({
-        center: userCoordinate,
+        center: userCoordinates,
         pointOfInterestFilter: filters,
-        radius: 3000,
+        radius: radius,
       })
 
       pointOfInterestSearch.search((error, data) => {
         if (error) {
           // TODO handle error
+          console.warn('error while searching ')
           return;
         }
 
-        // Place the users location on the map via a MarkerAnnotation
-        let userAnnotation = new mapkit.MarkerAnnotation(userCoordinate);
-        userAnnotation.color = "#f96345";
-        userAnnotation.glyphText = "🏠";
-        map.addAnnotation(userAnnotation);
+        if (data.places.length === 0){
+          //no places found increase the radius length by 1000
+          radius < 30000 ? setRadius(radius + 1000) : setStatus('Out of Luck Chief')
+        }
+        else {
+          // Place the users location on the map via a MarkerAnnotation
+          let userAnnotation = new mapkit.MarkerAnnotation(userCoordinates);
+          userAnnotation.color = "#f96345";
+          userAnnotation.glyphText = "🏠";
+          map.addAnnotation(userAnnotation);
 
-        // Pick a random place from the list of search results
-        let randomIndex = Math.floor(Math.random() * data.places.length);
-        setResults(data.places);
-        setStatus('Results found');
-        let randomPlace = data.places[randomIndex];
-        let randomPlaceAnnotation = new mapkit.MarkerAnnotation(
-          randomPlace.coordinate
-        );
-        randomPlaceAnnotation.color = "#5688d9";
-        randomPlaceAnnotation.title = randomPlace.name;
-        randomPlaceAnnotation.subtitle = randomPlace.formattedAddress;
+          // Pick a random place from the list of search results
+          let randomIndex = Math.floor(Math.random() * data.places.length);
+          setResults(data.places);
+          setStatus('Results found');
+          let randomPlace = data.places[randomIndex];
+          let randomPlaceAnnotation = new mapkit.MarkerAnnotation(
+            randomPlace.coordinate
+          );
+          randomPlaceAnnotation.color = "#5688d9";
+          randomPlaceAnnotation.title = randomPlace.name;
+          randomPlaceAnnotation.subtitle = randomPlace.formattedAddress;
 
-        // add the annotation to the map
-        setPlaceAnnotation(randomPlaceAnnotation);
-        map.addAnnotation(randomPlaceAnnotation);
+          // add the annotation to the map
+          setPlaceAnnotation(randomPlaceAnnotation);
+          map.addAnnotation(randomPlaceAnnotation);
 
-        let route = new mapkit.Directions().route(
-          {
-            origin: userCoordinate,
-            destination: randomPlace
-          },
-          (error, data) => {
-            let polylines = data.routes.map(route => {
-              return new mapkit.PolylineOverlay(route.polyline.points, {
-                style: new mapkit.Style({
-                  lineWidth: 5,
-                  strokeColor: "#139cc2"
+          let route = new mapkit.Directions().route(
+            {
+              origin: userCoordinates,
+              destination: randomPlace
+            },
+            (error, data) => {
+              let polylines = data.routes.map(route => {
+                return new mapkit.PolylineOverlay(route.polyline.points, {
+                  style: new mapkit.Style({
+                    lineWidth: 5,
+                    strokeColor: "#139cc2"
+                  })
+                });
+              });
+
+              setPath(polylines)
+
+              map.showItems(polylines, {
+                animate: true,
+                padding: new mapkit.Padding({
+                  top: 200,
+                  right: 56,
+                  bottom: 100,
+                  left: 56
                 })
               });
-            });
-
-            setPath(polylines)
-
-            map.showItems(polylines, {
-              animate: true,
-              padding: new mapkit.Padding({
-                top: 200,
-                right: 56,
-                bottom: 100,
-                left: 56
-              })
-            });
-          }
-        );
+            }
+          );
+        }
       });
     }
 
